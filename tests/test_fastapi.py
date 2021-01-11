2cb28fcc881c1c02ba488f882e071ab73a2fa584
grillazz <the@grillazz.com>
2021-01-11 19:22:00 +0100

    unit test with fake redis

---
+++ tests/test_fastapi.py
@@ -0,0 +1,20 @@
+import pytest
+from httpx import AsyncClient
+import fakeredis.aioredis
+from fastapi import status
+
+from fastapi_redis.main import app
+
+
+@pytest.fixture
+async def test_app():
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        server = fakeredis.FakeServer()
+        app.state.redis = await fakeredis.aioredis.create_redis_pool(server=server)
+        yield ac
+
+
+@pytest.mark.asyncio
+async def test_root(test_app):
+    response = await test_app.get("/health-check")
+    assert response.status_code == status.HTTP_200_OK
