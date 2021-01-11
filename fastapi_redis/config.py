5098a345fe1c9272637494c8370047dd19aa99d2
grillazz <the@grillazz.com>
2021-01-11 19:19:07 +0100

    app module

---
+++ fastapi_redis/config.py
@@ -0,0 +1,25 @@
+import logging
+import os
+from functools import lru_cache
+
+from pydantic import BaseSettings, AnyUrl
+
+
+log = logging.getLogger("uvicorn")
+
+
+class Settings(BaseSettings):
+    environment: str = os.getenv("ENVIRONMENT", "dev")
+    testing: str = os.getenv("TESTING", "0")
+    redis_url: AnyUrl = os.environ.get("REDIS_URL", "redis://redis")
+    redis_password: str = os.getenv("REDIS_PASSWORD", "redis_pass")
+    redis_db: int = int(os.getenv("REDIS_DB", "0"))
+    up: str = os.getenv("UP", "up")
+    down: str = os.getenv("DOWN", "down")
+    web_server: str = os.getenv("WEB_SERVER", "web_server")
+
+
+@lru_cache()
+def get_settings() -> BaseSettings:
+    log.info("Loading config settings from the environment...")
+    return Settings()
---
+++ fastapi_redis/main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI, Depends
+from aioredis import create_redis_pool, Redis
+
+
+from fastapi_redis import config
+
+global_settings = config.Settings()
+
+app = FastAPI()
+
+
+async def init_redis_pool() -> Redis:
+    redis = await create_redis_pool(
+        global_settings.redis_url,
+        password=global_settings.redis_password,
+        encoding="utf-8",
+        db=global_settings.redis_db,
+    )
+    return redis
+
+
+@app.on_event("startup")
+async def starup_event():
+    app.state.redis = await init_redis_pool()
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    app.state.redis.close()
+    await app.state.redis.wait_closed()
+
+
+@app.get("/health-check")
+async def health_check(settings: config.Settings = Depends(config.get_settings)):
+    try:
+        await app.state.redis.set(str(settings.redis_url), settings.up)
+        value = await app.state.redis.get(str(settings.redis_url))
+    except:  # noqa: E722
+        value = settings.down
+    return {settings.web_server: settings.up, str(settings.redis_url): value}
