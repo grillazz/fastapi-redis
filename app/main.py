105962fd95fe5adbf7949df4a0825a3282d84cbe
grillazz <the@grillazz.com>
2021-04-26 20:36:57 +0200

    redis hset poc

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Request
 from aioredis import create_redis_pool, Redis
 from app.schemas import CompoundsListSchema
 
 from app import config
 
+from rdkit.Chem import RDKFingerprint, MolFromSmiles
+from rdkit.Chem.Fingerprints.FingerprintMols import FingerprintMol, GetRDKFingerprint
+from rdkit.DataStructs import FingerprintSimilarity
+
 global_settings = config.Settings()
+
 
 app = FastAPI()
 
@@ -39,11 +44,17 @@ async def health_check(settings: config.Settings = Depends(config.get_settings))
         value = settings.down
     return {settings.web_server: settings.up, str(settings.redis_url): value}
 
-
 @app.post("/add-smiles")
-async def add_smiles(request: CompoundsListSchema):
-    return request
+async def add_smiles(payload: CompoundsListSchema):
 
+    mols = {}
+    for compound in payload.PC_Compounds:
+        mols.update({x.value.sval: str(RDKFingerprint(MolFromSmiles(x.value.sval)).ToBinary()) for x in compound.props})
+
+    for k, v in mols.items():
+        await app.state.redis.hset("mols:figers", k,v)
+
+    return await app.state.redis.hgetall("mols:figers")
 # 1. get list of SMILES
 # 2. loop over it d2 = {k: f(v) for k, v in d1.items()} like fps = [Chem.RDKFingerprint(x) for x in ms]
 # to build {k:v} for redis HSET
