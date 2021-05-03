7f332b9f815576d449793e897f1490c27d6ff2d1
grillazz <the@grillazz.com>
2021-05-03 18:12:10 +0200

    remove duplicate code

--- tests/test_api.py
+++ tests/test_api.py
@@ -10,4 +10,9 @@ async def test_set_and_compare(client, get_payload):
     response = await client.post(
         f"/api/smiles/add-to-hash/?redis_hash={redis_hash}", json=get_payload
     )
-    assert response.status_code == status.HTTP_200
+    assert response.status_code == status.HTTP_201_CREATED
+
+
+async def test_api(client):
+    response = await client.get("/health-check")
+    assert response.status_code == status.HTTP_200_OK
