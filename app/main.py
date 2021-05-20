a43bbac5d67b9f539e86c4b73fc8c751b9c19016
grillazz <the@grillazz.com>
2021-05-20 11:05:01 +0200

    swagger-ui monkey patch

--- app/main.py
+++ app/main.py
@@ -1,9 +1,27 @@
 from fastapi import Depends, FastAPI
+from fastapi import FastAPI, applications
+from fastapi.openapi.docs import get_swagger_ui_html
 
 from app import config
 from app.redis import init_redis_pool
 from app.routers import smiles_router
 from app.service import MoleculesRepository
+
+def swagger_monkey_patch(*args, **kwargs):
+    """
+    Wrap the function which is generating the HTML for the /docs endpoint and
+    overwrite the default values for the swagger js and css.
+    """
+    return get_swagger_ui_html(
+        *args,
+        **kwargs,
+        swagger_js_url="https://cdn.jsdelivr.net/npm/swagger-ui-dist@3.48.0/swagger-ui-bundle.js",
+        swagger_css_url="https://cdn.jsdelivr.net/npm/swagger-ui-dist@3.48.0/swagger-ui.css"
+    )
+
+
+# Actual monkey patch
+applications.get_swagger_ui_html = swagger_monkey_patch
 
 global_settings = config.Settings()
 
