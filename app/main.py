756df32368597f122ff3418ac09d6ca04e02178e
grillazz <the@grillazz.com>
2021-06-04 14:23:13 +0200

    update project dependencies and remove monkey patch

--- app/main.py
+++ app/main.py
@@ -7,22 +7,6 @@ from app.redis import init_redis_pool
 from app.routers import smiles_router
 from app.service import MoleculesRepository
 
-def swagger_monkey_patch(*args, **kwargs):
-    """
-    Wrap the function which is generating the HTML for the /docs endpoint and
-    overwrite the default values for the swagger js and css.
-    """
-    return get_swagger_ui_html(
-        *args,
-        **kwargs,
-        swagger_js_url="https://cdn.jsdelivr.net/npm/swagger-ui-dist@3.48.0/swagger-ui-bundle.js",
-        swagger_css_url="https://cdn.jsdelivr.net/npm/swagger-ui-dist@3.48.0/swagger-ui.css"
-    )
-
-
-# Actual monkey patch
-applications.get_swagger_ui_html = swagger_monkey_patch
-
 global_settings = config.Settings()
 
 
